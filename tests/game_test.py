438f70c7f74bf661c648db873bbd573afe025e3c
Muzzi Aldean <amiraldean@gmail.com>
2018-07-23 08:32:03 +0100

    Current player stored as attribute

--- tests/game_test.py
+++ tests/game_test.py
@@ -8,3 +8,7 @@ class TestGame(object):
     def test_has_an_array_of_positions(self):
         game = Game()
         assert game.positions == ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+    def test_has_current_player_attribute(self):
+        game = Game()
+        assert game.current_player == 'X'
